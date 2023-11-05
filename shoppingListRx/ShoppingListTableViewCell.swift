fdf1fe1a9214b17ec9de385b1c50a9b4849791f8
한 <hih458@naver.com>
2023-11-05 21:51:22 +0900

    feat 기본 UI생성

---
+++ shoppingListRx/ShoppingListTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  ShoppingListTableViewCell.swift
+//  shoppingListRx
+//
+//  Created by 황인호 on 11/5/23.
+//
+
+import Foundation
---
+++ shoppingListRx/ShoppingListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ShoppingListViewModel.swift
+//  shoppingListRx
+//
+//  Created by 황인호 on 11/5/23.
+//
+
+import Foundation
